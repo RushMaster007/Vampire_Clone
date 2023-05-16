86a6f2c0b266f0c1ea1b77e98fa4c241ce0249ec
Earthychen <132041225+Earthychen@users.noreply.github.com>
2023-05-16 19:53:28 +0200

    Added Dash

--- Survivors Clone/Player/player.gd
+++ Survivors Clone/Player/player.gd
@@ -1,7 +1,14 @@
 extends CharacterBody2D
 
+# Time is measured in 1/60th of a second. -> 60 Frames = 1 Second
 
-var movement_speed = 50.0
+const MAX_DASH_DURATION = 60 # => 1s
+const MAX_DASH_TIMEOUT = 150 # => 2.5s
+
+var movement_speed = 60.0
+
+var dash_duration_timer = MAX_DASH_DURATION
+var dash_timeout_timer = MAX_DASH_TIMEOUT
 
 #func _process(delta) ex 1/3000 
 
@@ -16,6 +23,23 @@ func process_movement():
 	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up") #  Up: negative, down: poistive
 	var mov = Vector2(x_mov, y_mov);
 	
-	velocity = mov.normalized() * movement_speed 
+	#Dash-Functionality
+	if Input.get_action_strength("dash") == 1 && dash_timeout_timer >= MAX_DASH_TIMEOUT:
+		dash_duration_timer = 0
+		dash_timeout_timer = 0
+		
+	var dash_multiplier = 1
+	
+	# Count Timers
+	if dash_duration_timer < MAX_DASH_DURATION:
+		dash_duration_timer = dash_duration_timer + 1
+		dash_multiplier = 5 # Set Multiplier
+	
+	if dash_timeout_timer < MAX_DASH_TIMEOUT:
+		dash_timeout_timer = dash_timeout_timer + 1
+		
+	
+		
+	velocity = mov.normalized() * movement_speed * dash_multiplier
 	
 	move_and_slide() # Tells game to actually move the player body
