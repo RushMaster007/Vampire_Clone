255da5437fbf77528f13c1bc46915c11318c39e7
Earthychen <132041225+Earthychen@users.noreply.github.com>
2023-06-12 17:03:00 +0200

    Update player.gd
    Delete Dash-Functionality

--- Survivors Clone/Player/player.gd
+++ Survivors Clone/Player/player.gd
@@ -1,14 +1,6 @@
 extends CharacterBody2D
 
-# Time is measured in 1/60th of a second. -> 60 Frames = 1 Second
-
-const MAX_DASH_DURATION = 60 # => 1s
-const MAX_DASH_TIMEOUT = 150 # => 2.5s
-
 var movement_speed = 60.0
-
-var dash_duration_timer = MAX_DASH_DURATION
-var dash_timeout_timer = MAX_DASH_TIMEOUT
 
 #func _process(delta) ex 1/3000 
 
@@ -16,30 +8,16 @@ func _physics_process(delta): # 1/60
 	process_movement()
 	
 func process_movement():
-	# TODO: Motion Mode: Floating
-	# TODO: ProjectSettings -> Input-Map
 	
+	# Calculate x- and y movement
 	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up") #  Up: negative, down: poistive
+		
+	# Create Movement-Vector
 	var mov = Vector2(x_mov, y_mov);
 	
-	#Dash-Functionality
-	if Input.get_action_strength("dash") == 1 && dash_timeout_timer >= MAX_DASH_TIMEOUT:
-		dash_duration_timer = 0
-		dash_timeout_timer = 0
-		
-	var dash_multiplier = 1
+	# Calculate and set player velocity
+	velocity = mov.normalized() * movement_speed
 	
-	# Count Timers
-	if dash_duration_timer < MAX_DASH_DURATION:
-		dash_duration_timer = dash_duration_timer + 1
-		dash_multiplier = 5 # Set Multiplier
-	
-	if dash_timeout_timer < MAX_DASH_TIMEOUT:
-		dash_timeout_timer = dash_timeout_timer + 1
-		
-	
-		
-	velocity = mov.normalized() * movement_speed * dash_multiplier
-	
-	move_and_slide() # Tells game to actually move the player body
+	# Tells game to actually move the player body
+	move_and_slide() 
